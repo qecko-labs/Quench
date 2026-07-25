89b1a636fdba01e3b57e838944faed074c0c6a11
alexvoste <alexvostedev@proton.me>
2026-07-25 10:27:36 -0400

    chore(rebrand): replace ForgeZero/fz with Quench/qh in build.sh
    Signed-off-by: alexvoste <alexvostedev@proton.me>

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go build -ldflags="-X github.com/forgezero-cli/ForgeZero/cmd/fz/cli.BuildDate=$(date +%Y-%m-%d) -X github.com/forgezero-cli/ForgeZero/cmd/fz/cli.VersionCore=v6.0.0" -o fz cmd/fz/main.go
+go build -ldflags="-X github.com/forgezero-cli/ForgeZero/cmd/fz/cli.BuildDate=$(date +%Y-%m-%d) -X github.com/forgezero-cli/ForgeZero/cmd/fz/cli.VersionCore=v6.0.0" -o qh cmd/fz/main.go
